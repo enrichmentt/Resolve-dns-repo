5edcd1de361dc828e9ecc8efb3e76f98bc2b8a96
Vladimir Pekshev <enrichment.off@gmail.com>
2021-01-19 13:35:54 +0300

    first commit

---
+++ resolve.ps1
@@ -0,0 +1,22 @@
+﻿$names = Get-Content .\names.txt
+$result = @()
+
+foreach ($name in $names) {
+    $resolve = Resolve-DnsName $name
+
+    foreach ($res in $resolve) {
+        if($res.Type -eq 'A'){
+            $name
+            $res
+
+            $result += [PSCustomObject]@{
+                Name = $name
+                DnsName = $res.Name
+                TTL = $res.TTL
+                IP = $res.IPAddress
+            }
+        }
+    }
+}
+
+$result | Format-Table
